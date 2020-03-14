0095bc4030c5d4749bcee1f29f2812e8efcfc08f
eportilla <marsarf@gmail.com>
2020-03-14 12:31:47 -0700

    Adding jest types for testing

--- src/app/vginventory/src/components/test/UserProfile.test.tsx
+++ src/app/vginventory/src/components/test/UserProfile.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { UserProfile } from '../UserProfile';
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('renders learn react link', () => {
   const { getByText } = render(<UserProfile />);
