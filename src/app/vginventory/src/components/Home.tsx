c259bdca152583273b8b8cce75e894f527741e5b
eportilla <marsarf@gmail.com>
2020-03-14 21:36:35 -0700

    Adding router and home page

---
+++ src/app/vginventory/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Video Game Inventory</h1>
+        <p>Welcome to the video game inventory.</p>
+    </div>
+  );
+}
