ee1b1a5c6e5ba3776f3722f4380eb0616b359b3c
eportilla <marsarf@gmail.com>
2020-03-15 17:07:32 -0700

    Fixed the app test

--- src/app/vginventory/src/App.test.js
+++ src/app/vginventory/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('User1');
+  const { getByText } = render(<MemoryRouter initialEntries={["/"]}><App /></MemoryRouter>);
+  const linkElement = getByText('VGinventory');
   expect(linkElement).toBeInTheDocument();
 });
