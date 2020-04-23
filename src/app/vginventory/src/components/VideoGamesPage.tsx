07a6e5a0f0d25c2bf2527a17f54cba7a66934cda
eportilla <marsarf@gmail.com>
2020-04-23 12:57:21 -0700

    Updating the card style

--- src/app/vginventory/src/components/VideoGamesPage.tsx
+++ src/app/vginventory/src/components/VideoGamesPage.tsx
@@ -7,13 +7,13 @@ export const VideoGamesPage = (props: any) => {
     const { videoGames } = useVideoGames(props.games);
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
             {videoGames.map((vg) => (
                 <div key={vg.title}>
-                    <Card style={{width: '18em', flex: '1'}}>
+                    <Card style={{width: '18em', flex: '1', margin: '1em', height: '18em'}}>
                         <Card.Header as="h5">{vg.title}</Card.Header>
-                        <Card.Body>
-                            <Card.Text>{vg.description}</Card.Text>
+                        <Card.Body style={{overflow: 'hidden'}}>
+                            <Card.Text style={{wordWrap: 'break-word'}}>{vg.description}</Card.Text>
                         </Card.Body>
                         <Card.Footer><Link to={`/videogames/${vg.title}`}>Go to</Link></Card.Footer>
                     </Card>
