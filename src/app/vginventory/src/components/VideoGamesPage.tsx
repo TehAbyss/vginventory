03ab51d437352fc9052f86da1403089f356d6d0e
eportilla <marsarf@gmail.com>
2020-04-23 14:02:52 -0700

    moving styles to styles/css files

--- src/app/vginventory/src/components/VideoGamesPage.tsx
+++ src/app/vginventory/src/components/VideoGamesPage.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { useVideoGames } from '../hooks/useVideoGames';
 import { Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import './../styles/videoGames.css';
 
 export const VideoGamesPage = (props: any) => {
     const { videoGames } = useVideoGames(props.games);
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+        <div className="VG-div">
             {videoGames.map((vg) => (
                 <div key={vg.title}>
-                    <Card style={{width: '18em', flex: '1', margin: '1em', height: '18em'}}>
+                    <Card className="VG-card">
                         <Card.Header as="h5">{vg.title}</Card.Header>
-                        <Card.Body style={{overflow: 'hidden'}}>
-                            <Card.Text style={{wordWrap: 'break-word'}}>{vg.description}</Card.Text>
+                        <Card.Body className="VG-card-body">
+                            <Card.Text className="VG-card-text">{vg.description}</Card.Text>
                         </Card.Body>
                         <Card.Footer><Link to={`/videogames/${vg.title}`}>Go to</Link></Card.Footer>
                     </Card>
