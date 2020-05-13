3ecd94c24016f6cc417a551461b1e04d54eccf1f
eportilla <marsarf@gmail.com>
2020-05-12 20:58:40 -0700

    Updating the video game edit page

--- src/app/vginventory/src/components/VideoGameEdit.tsx
+++ src/app/vginventory/src/components/VideoGameEdit.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { useUpdateVideoGame } from '../hooks/useUpdateVideoGame';
+import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export const VideoGameEdit = (props: any) => {
     const { videogame, description, onDescriptionChange, onUpdate } = useUpdateVideoGame(props.location.state.game);
     
     return (
-        <>
-            <h1>{videogame.title}</h1>
-            <form>
-                <label>
-                    Description:
-                <textarea value={description} onChange={onDescriptionChange} />
-                </label>
-                <p><input type="submit" value="Update" onClick={onUpdate} /></p>
-            </form>
-        </>
+        <div className="main">
+            <div className="section section-dark">
+                <Container>
+                    <h1>{videogame.title}</h1>
+                    <Form>
+                        <FormGroup>
+                            <Label for="description">Description:</Label>
+                            <Input type="textarea" id="description" value={description} onChange={onDescriptionChange} />
+                        </FormGroup>
+                        <Button onClick={onUpdate}>Update</Button>
+                    </Form>
+                </Container>
+            </div>
+        </div>
     )
 }
