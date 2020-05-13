e8c364a0ef12b14177e93327f3947ed95ed42829
eportilla <marsarf@gmail.com>
2020-05-12 20:49:00 -0700

    Added style to video game profile

--- src/app/vginventory/src/components/VideoGameProfile.tsx
+++ src/app/vginventory/src/components/VideoGameProfile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useGetVideoGame } from "../hooks/useGetVideoGame";
 import { useParams, useHistory } from "react-router-dom";
+import { Container, Button } from 'reactstrap';
 
 export const VideoGameProfile = () => {
     let { title } = useParams();
@@ -10,11 +11,15 @@ export const VideoGameProfile = () => {
     if (!videogame) return null;
 
     return (
-        <div>
-
-            <h1>{videogame.title}</h1>
-            <p>{videogame.description}</p>
-            <button type="button" onClick={() => history.push(`/videogames/${videogame.title}/edit`, {game: videogame})}>Edit</button>
+        <div className="main">
+            <div className="section section-dark">
+                <Container>
+                    <h1>{videogame.title}</h1>
+                    <h3>{videogame.description}</h3>
+                    <br/>
+                    <Button onClick={() => history.push(`/videogames/${videogame.title}/edit`, {game: videogame})}>Edit</Button>
+                </Container>
+            </div>
         </div>
     )
 };
