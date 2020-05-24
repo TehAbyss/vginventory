4654d225b43390278793b5652e8b62edfba96e54
eportilla <marsarf@gmail.com>
2020-05-24 15:51:32 -0700

    Adding header to page

--- src/app/vginventory/src/components/VideoGamesPage.tsx
+++ src/app/vginventory/src/components/VideoGamesPage.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import { useVideoGames } from '../hooks/useVideoGames';
-import { Container, Card, CardHeader, CardBody, CardText, CardFooter, Button, CardDeck } from "reactstrap";
+import { Container, Card, CardHeader, CardBody, CardText, CardFooter, Button, CardDeck, Row, Col } from "reactstrap";
 import './../styles/videoGames.css';
 
 export const VideoGamesPage = (props: any) => {
     const { videoGames } = useVideoGames(props.games);
+    let pageHeader = React.createRef<HTMLDivElement>();
+
+    React.useEffect(() => {
+        if (window.innerWidth < 991) {
+        const updateScroll = () => {
+            let windowScrollTop = window.pageYOffset / 3;
+            if (pageHeader.current) {
+                pageHeader.current.style.transform =
+                "translate3d(0," + windowScrollTop + "px,0)";
+            }
+        };
+        window.addEventListener("scroll", updateScroll);
+        return function cleanup() {
+            window.removeEventListener("scroll", updateScroll);
+        };
+        }
+    });
 
     return (
-        <div className="main">
+        <>
+            <div
+                className="page-header page-header-xs section-dark"
+                style={{
+                backgroundImage:
+                    "url(" + require("../img/daniel-olahh.jpg") + ")"
+                }}
+                data-parallax={true}
+                ref={pageHeader} />
+            
             <div className="section section-dark">
                 <Container>
+                    <div className="title">
+                        <h2>Video Games</h2>
+                    </div>
+                    <Row>
+                        <Col md="8">
+                            <Button href="" color="default">Add video Game</Button>
+                        </Col>
+                    </Row>
                     <CardDeck>
                         {videoGames.map((vg) => (
                             <div key={vg.title}>
@@ -25,6 +59,6 @@ export const VideoGamesPage = (props: any) => {
                     </CardDeck>
                 </Container>
             </div>
-        </div>
+        </>
     )
 };
